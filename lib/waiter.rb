5e088295933fbd2d07c6f1331eca5c1bd096bdb2
Ben Altman <altmanguitar@gmail.com>
2021-01-27 01:06:07 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -25,12 +25,12 @@ class Waiter
     Meal.all.select {|m| m.waiter == self}
   end
 
-#  def best_tipper
-#    best_tipped_meal = meals.max.do |meal_a, meal_b|
-#      meal_a.tip <=> meal_b.tip
-#    end
-#
-#    best_tipped_meal.customer
-#  end
+  def best_tipper
+    best_tipped_meal = meals.max.do |meal_a, meal_b|
+      meal_a.tip <=> meal_b.tip
+    end
+
+    best_tipped_meal.customer
+  end
 
 end
